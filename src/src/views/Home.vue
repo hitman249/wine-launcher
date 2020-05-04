86f24f69b093e3f46869d859e92301c549a66222
Neiron <hitman2491@gmail.com>
2020-05-04 19:21:56 +0700

    info

--- src/src/views/Home.vue
+++ src/src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="row">
         <div class="col-lg-12">
+            <div class="grid-structure" v-if="!games.configs.length">
+                <div class="grid-container">
+                    * Чтобы установить игру создайте
+                    <router-link to="/patches">новый патч</router-link>
+                    и выберите <code>Операции > Установить приложение</code>
+                    <br>
+                    * Уже установленные игры можно просто переместить в папку <code>./data/games</code>
+                    <br>
+                    * После установки создайте к игре
+                    <router-link to="/games">ярлык</router-link>
+                    <br>
+                    * Больше информации можно прочитать в
+                    <router-link to="/help">справке</router-link>
+                </div>
+            </div>
+
             <ItemNewGame v-if="!games.configs.length"/>
             <ItemGame v-for="config in games.configs" :key="config.code" :config="config" :edit="false"/>
         </div>
@@ -30,3 +46,15 @@
         methods:    {},
     }
 </script>
+
+<style lang="less" scoped>
+    .grid-structure {
+        border: 2px solid #ffbd4a;
+        margin-bottom: 20px;
+    }
+
+    .grid-container {
+        padding: 20px;
+        margin: 0;
+    }
+</style>
